564b65805e56e3f479fbeabc568385e637e67795
Rahul gitweb <rahuljackok20@gmail.com>
2026-08-14 14:32:42 +0530

    Fix production authentication middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,25 +1,6 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-const accessToken = request.cookies.get("accessToken")?.value;
-
-  const isAuthPage = request.nextUrl.pathname.startsWith("/authenticate");
-  const isDashboard = request.nextUrl.pathname.startsWith("/dashboard");
-
-
-  if (!accessToken && isDashboard) {
-    return NextResponse.redirect(
-      new URL("/authenticate/login", request.url)
-    );
-  }
-
-
-  if (accessToken && isAuthPage) {
-    return NextResponse.redirect(
-      new URL("/dashboard", request.url)
-    );
-  }
-
   return NextResponse.next();
 }
 
